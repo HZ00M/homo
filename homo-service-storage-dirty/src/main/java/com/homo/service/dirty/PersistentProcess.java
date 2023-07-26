69f08962efdc70800c949933d68319c06767114d
hzm <1054743949@qq.com>
2023-07-26 22:59:52 +0800

    1、优化代码结构 2、优化异常处理

--- homo-service-storage-dirty/src/main/java/com/homo/service/dirty/PersistentProcess.java
+++ homo-service-storage-dirty/src/main/java/com/homo/service/dirty/PersistentProcess.java
@@ -38,6 +38,6 @@ public class PersistentProcess implements Module {
                     log.info("landingTask unlock dirtyName end{} ",dirtyName);
                     init();
                 }
-            }, dirtyProperties.getDelayTime());
+            }, dirtyProperties.getDelayTimeMillSecond());
     }
 }
